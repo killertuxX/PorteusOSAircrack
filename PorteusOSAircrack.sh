fd43f009901e4b90178a38137373711006ee8f7a
killertuxX <martin.kardos2000@hotmail.com>
2018-09-23 20:20:47 +0200

    Add files via upload

---
+++ PorteusOSAircrack.sh
@@ -0,0 +1,11 @@
+wget http://ftp.br.debian.org/debian/pool/main/a/aircrack-ng/aircrack-ng_1.3-3_i386.deb
+deb2xzm aircrack-ng_1.3-3_i386.deb
+activate aircrack-ng_1.3-3_i386.xzm
+wget http://ftp.us.debian.org/debian/pool/main/i/iw/iw_4.14-0.1_i386.deb
+deb2xzm iw_4.14-0.1_i386.deb 
+activate iw_4.14-0.1_i386.xzm
+wget http://ftp.altlinux.org/pub/distributions/ALTLinux/Sisyphus/i586/RPMS.classic//aircrack-ng-1.3-alt1.i586.rpm
+rpm2xzm aircrack-ng-1.3-alt1.i586.rpm
+activate aircrack-ng-1.3-alt1.i586.xzm
+
+
